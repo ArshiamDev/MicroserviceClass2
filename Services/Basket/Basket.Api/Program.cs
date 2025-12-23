c6696116c17a8fcecbb174f8258f5d74f9d830c6
ArshiamDev <arshiam.dev@gmail.com>
2025-12-23 16:52:46 +0330

    session 16 complete redis

---
+++ Services/Basket/Basket.Api/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ? Redis Cache
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+	options.Configuration =
+		builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
